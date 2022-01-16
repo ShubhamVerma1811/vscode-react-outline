38cdd6bfb726444267711933fea5fc64e004697f
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2022-01-16 13:08:55 +0530

    chore: support for multiple elements, fragments, nested nodes

--- src/parser.ts
+++ src/parser.ts
@@ -1,57 +1,125 @@
 import { parse } from "@babel/parser";
 import traverse, { NodePath } from "@babel/traverse";
-import { JSXElement } from "@babel/types";
+import { JSXElement, JSXFragment, JSXMemberExpression } from "@babel/types";
 import { DocumentSymbol, Position, Range, SymbolKind } from "vscode";
 
-let firstJSXElement: NodePath<JSXElement>["node"];
+type Node = JSXElement | JSXFragment | JSXMemberExpression;
 
 export const getSymbolTree = (code: string): DocumentSymbol[] => {
+  const nodes: Node[] = [];
   const ast = parse(code, {
     sourceType: "module",
     plugins: ["jsx", "typescript"]
   });
 
-  // TODO: Look for better way, maybe traverse recursively inside the preOrder?
   traverse(ast, {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     JSXElement(path) {
-      firstJSXElement = path.node;
+      nodes.push(path.node);
+      path.stop();
+    },
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    JSXFragment(path) {
+      nodes.push(path.node);
       path.stop();
     }
+    // TODO: handle JSXExpression, ConditionalExpression, LogicalExpression
   });
 
-  const symbols = preOrderTraversal(firstJSXElement);
-
-  return [symbols];
+  const symbols = parseDocumentSymbols(nodes);
+  return symbols;
 };
 
-// TODO: handle for Fragement and JSXExpressions
-const preOrderTraversal = (node: NodePath<JSXElement>["node"]) => {
-  let symbol: DocumentSymbol;
-  const { openingElement, loc, children } = node;
+const generateSymbolTree = (
+  node: NodePath<JSXElement>["node"]
+): DocumentSymbol => {
+  const { openingElement, loc } = node;
+  if (!loc) {
+    throw new Error("No location");
+  }
 
-  const range = new Range(
-    // @ts-ignore
-    new Position(loc?.start.line, loc?.start.column),
-    // @ts-ignore
+  const position = new Position(loc?.start.line - 1, loc?.start.column - 1);
+  const range = new Range(position, position);
 
-    new Position(loc?.start.line, loc?.start.column)
-  );
+  const name = () => {
+    if (openingElement.name.type === "JSXMemberExpression") {
+      // @ts-ignore
+      return `${openingElement.name.object.name}.${openingElement.name.property.name}`;
+    } else {
+      return openingElement.name.name;
+    }
+  };
 
-  symbol = new DocumentSymbol(
+  return new DocumentSymbol(
+    // TODO: fix types
     // @ts-ignore
-    openingElement.name.name,
+    name(),
     "",
     SymbolKind.Variable,
     range,
     range
   );
+};
 
-  for (const child of children) {
-    if (child.type === "JSXElement") {
-      const response = preOrderTraversal(child);
-      symbol.children.push(response);
+const parseDocumentSymbols = (nodes: Node[]): DocumentSymbol[] => {
+  const symbols: DocumentSymbol[] = [];
+  nodes.forEach((node) => {
+    if (node.type === "JSXElement") {
+      const symbol = parseJSXElement(node);
+      symbols.push(symbol);
+    } else if (node.type === "JSXFragment") {
+      const symbol = parseJSXFragment(node);
+
+      symbols.push(symbol);
     }
-  }
+  });
+
+  return symbols;
+};
+
+const parseJSXElement = (node: JSXElement) => {
+  let symbol: DocumentSymbol;
+  const { children } = node;
+
+  symbol = generateSymbolTree(node);
+  parseChildren<typeof children>(symbol, children);
 
   return symbol;
 };
+
+// TODO: refactor
+const parseJSXFragment = (node: JSXFragment): DocumentSymbol => {
+  if (!node.loc) {
+    throw new Error("No LOC");
+  }
+  const position = new Position(
+    node.loc?.start.line - 1,
+    node.loc?.start.column - 1
+  );
+  const range = new Range(position, position);
+
+  let symbol: DocumentSymbol = new DocumentSymbol(
+    "Fragment",
+    "",
+    SymbolKind.Field,
+    range,
+    range
+  );
+
+  parseChildren<typeof node.children>(symbol, node?.children);
+  return symbol;
+};
+
+function parseChildren<T extends Array<any>>(
+  symbol: DocumentSymbol,
+  _children: T
+) {
+  for (const child of _children) {
+    if (child.type === "JSXElement") {
+      const response = parseJSXElement(child);
+      symbol.children.push(response);
+    } else if (child.type === "JSXFragment") {
+      symbol.children.push(parseJSXFragment(child));
+    }
+  }
+}
